d1e3d80ce7575931257f0ad530d6779f363c878d
Your Name <carlosvanzego96@gmail.com>
2025-01-24 17:56:12 -0600

    Edits

--- notes.py
+++ notes.py
@@ -15,9 +15,8 @@ print(type(x))
 # In the terminal this prints "<class 'str'>", this is due to the same reasoning.
 
 
-# What this all means is that everything in Python is an Object of some type of Class.
-# These are called "built-in" types these are built into the python language; They work differntly than normal classes.
-# Whenever you create something in Python, you are really creating an Object, which is an instance of a specific Class; That Class defines the way the Object can interact with other things in the program.
+# What this all means is that everything in Python is an Object of some type of Class; Whenever you create something in Python, you are really creating an Object, which is an instance of a specific Class; That Class defines the way the Object can interact with other things in the program.
+# These are called "built-in" types these are built into the python language; They work differently than normal classes.
 
 
 # Methods - A function that goes inside of a class; Can be performed on Objects.
