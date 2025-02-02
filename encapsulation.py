15a69cc3994f908afa1389540cbfd4bf80b5d0ce
Your Name <carlosvanzego96@gmail.com>
2025-02-01 18:05:54 -0600

    encapsulation.py comments

--- encapsulation.py
+++ encapsulation.py
@@ -1,10 +1,11 @@
-
+# this is a class I called "Car".
 class Car:
-     
+     # This is the constructor method with the argument "self", "make", "model", "color". I set them each to a default value.
      def __init__ (self, make="Volkswagen", model="Passat", color="Blue"):
-        self.color
-        self.make
-        self.model
+        # then I am initializing the "make", "model" and "color" attributes.
+        self.color = color
+        self.make = make
+        self.model = model
 
      def start(self):
         print(f"The {self.color} {self.make} {self.model} is starting.")
