d19df11d5878dad676df12fdb9c94584487e973b
Your Name <carlosvanzego96@gmail.com>
2025-02-03 20:07:57 -0600

    variables.py update

--- variables.py
+++ variables.py
@@ -2,13 +2,14 @@
 
 # this is a variable I created called "x" inside it I stored the value 'Los'. By doing this I am making the "x" variable of type str (string).
 x = 'Los'
+y = 'Carlos'
 # this is another variable, I called this one "a" and stored the value 4. By doing this I am making "4" of type int (integer).
 a = 4
-
+b = 8
 
 # this is a print statement I am using to print the values of variable "X" and variable "a"
 print(x,a)
-
+print(y,b)
 
 
 
