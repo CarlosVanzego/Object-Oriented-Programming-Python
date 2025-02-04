c914d00ba8513c11c5ae2f9414dc91e446225654
Your Name <carlosvanzego96@gmail.com>
2025-02-04 07:40:40 -0600

    New variables in variables.py and comments

--- variables.py
+++ variables.py
@@ -3,13 +3,16 @@
 # this is a variable I created called "x" inside it I stored the value 'Los'. By doing this I am making the "x" variable of type str (string).
 x = 'Los'
 y = 'Carlos'
+# This varaible "F" is a constant, you know this because I used a capital letter to name the variable. When this is done it is using a naming convention that indicates that the variable should be treated as a constant and not changed.
+F = 'Full Metal'
 # this is another variable, I called this one "a" and stored the value 4. By doing this I am making "4" of type int (integer).
 a = 4
 b = 8
+A = 'alchemist'
 
 # this is a print statement I am using to print the values of variable "X" and variable "a"
-print(x,a)
-print(y,b)
+print(F)
+print(A)
 
 
 
