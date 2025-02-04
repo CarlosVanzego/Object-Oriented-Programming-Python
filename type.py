3eb9963d489d0f806f3c29d64eb0312c59d7d254
Your Name <carlosvanzego96@gmail.com>
2025-02-03 20:30:38 -0600

    type.py and comments

---
+++ type.py
@@ -0,0 +1,34 @@
+# Type Keyword
+# The built-in function type() returns the type of an object. In Python3, everything is an object and thus an instance of a class. Therefore, the built-in function type() can be used to return the class type of the passed variable.
+# Ex
+
+
+# output: <class 'int'> because I passed an integer.
+type(4)
+print(type(4))
+
+# output: <class 'str'> because I passed a string.
+type("Hello there.")
+print(type("Hello there."))
+
+# output: <class 'dict'> because I passed a dictionary.
+type({"age": 28})
+print(type({"age": 28}))
+
+# output: <class 'list'> because I passed a list.
+type((1,2,3,4))
+print(type(1,2,3,4))
+
+# output: <class 'float'> because I passed a float.
+type(2.0)
+print(type(2.0))
+
+# output: <class 'bool> because I passed a boolean.
+type(True)
+print(type(True))
+
+# output: <class 'NonneType'> because I passed None.
+type(None)
+print(type(None))
+
+
