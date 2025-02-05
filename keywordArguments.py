ab5e274c7ba78e25e0c1400e8135225ad9851a91
Your Name <carlosvanzego96@gmail.com>
2025-02-05 10:33:40 -0600

    keywordArguments.py and comments

---
+++ keywordArguments.py
@@ -0,0 +1,21 @@
+# Keyword Arguments:
+# Keyword Arguments are arguments that can be passed with the key = value syntax. This way the order of the arguments does not matter. 
+# Ex.
+# 
+def findYoungestKid(kid4, kid3, kid2, kid1):
+  print("The youngest kid is " + kid4)
+
+findYoungestKid(kid1 = "Carlos", kid2 = "Taylor", kid3 = "Robert", kid4 = "Nia")  
+
+
+
+# Arbitrary Keyword Arguments:
+# If the number of keyword arguments to be passed to a function is unknown, two asterisks (**) are placed before the parameter name in the functions definition. 
+# This approach allows the function to reciieve the arguments as a dictionary enabling access to each item by key.
+# Ex.
+# 
+def printLastName(**kid):
+  print("Her last name is " + kid["lname"])
+
+printLastName(fname = "Nia", lname = "Hall") 
+ 
