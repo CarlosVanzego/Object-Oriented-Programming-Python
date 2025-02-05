32ccf70866610254703c6a31762834142af19f7d
Your Name <carlosvanzego96@gmail.com>
2025-02-05 10:44:58 -0600

    keywordArguments.py edits

--- keywordArguments.py
+++ keywordArguments.py
@@ -11,11 +11,11 @@ findYoungestKid(kid1 = "Carlos", kid2 = "Taylor", kid3 = "Robert", kid4 = "Nia")
 
 # Arbitrary Keyword Arguments:
 # If the number of keyword arguments to be passed to a function is unknown, two asterisks (**) are placed before the parameter name in the functions definition. 
-# This approach allows the function to reciieve the arguments as a dictionary enabling access to each item by key.
+# This approach allows the function to receieve the arguments as a dictionary enabling access to each item by key.
 # Ex.
 # 
 def printLastName(**kid):
   print("Her last name is " + kid["lname"])
 
 printLastName(fname = "Nia", lname = "Hall") 
- 
+
