7db88fd54bd214aef0a182412f8b8083ddb13a5c
Your Name <carlosvanzego96@gmail.com>
2025-02-06 14:23:45 -0600

    sets.py update and comments

--- sets.py
+++ sets.py
@@ -3,14 +3,17 @@
 # A few characteristics of Sets:
 # Sets do not have a proper order. The items in a set are arranged in a random order.
 # The items in a Set do not have an index.
-# Sets cannot have two items with the sanme value.
+# Sets cannot have two items with the same value.
 # Sets are used when you want to store a set of unique values and perform venn diagram like operations on it for example union, intersection etc.
 # 
 # Sets cannot have duplicates as illustrated below:
 
-cities  = {"new york", "chicago", "kansas", "chicago"}
+cities = {"new york", "chicago", "kansas", "chicago"}
 #  When printing the cities set, the 2nd "chicago" does not show in the terminal. This is because sets cannot have duplicates.
 print(cities)
+
+states = {"Texas", "Maryland", "Louisiana", "California"} 
+print(states)
 
 cities = {"new york", "chicago", "kansas"}
 # this prints the length of the cities set. Which is 3.
