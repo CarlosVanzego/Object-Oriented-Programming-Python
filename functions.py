cf6ffe5129d057527125bb03f5beb0df08d005a8
Your Name <carlosvanzego96@gmail.com>
2025-02-06 13:56:46 -0600

    functions.py updates and comments

--- functions.py
+++ functions.py
@@ -1,5 +1,5 @@
-# Funcitons are predefined tasks created by the programmer to be used later in the program.
-# They help in breaking down the program into smaller chunks, making the code more organized and reusable.
+# Functions are predefined tasks created by the programmer to be used later in a program.
+# They help in breaking down the program into smaller, more modular chunks, making the code more organized and reusable.
 # For example you can write a function to draw a square and then call this function whenever you need to draw a square, instead of writing the square-drawing code each time.
 # Ex.
 
@@ -19,3 +19,6 @@ def practice_function():
     return t
     
 print(practice_function())
+# This is a function called 'spell_name'. I have pass on the line beneath the function declaration to keep it syntactically correct.
+def spell_name():
+    pass
