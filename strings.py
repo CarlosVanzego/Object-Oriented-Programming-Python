880e8a58538be3f0ecc9119a54eded608ac58c10
Your Name <carlosvanzego96@gmail.com>
2025-02-05 18:35:39 -0600

    strings.py and comments

---
+++ strings.py
@@ -0,0 +1,24 @@
+# Strings
+# A String can be a collection of alphabets, words, or other characters. They are typically used to store values like names, places or any other combination of alphanumeric or special characters.
+
+# Strings in Python are "immutable" which means they cannot be changed after they are created. You cannot insert or remove a charachter from a string after it is created.
+
+# Strings in Python are surrounded by either single quotation marks or double quotation marks.
+# 
+# Syntax: 
+str = "Hello" 
+
+print(str)
+
+# String length:
+str = "Hello there."
+
+print(len(str))
+
+# String concatenation:
+word1 = "Hello"
+word2 = "World"
+
+sentence = word1 + word2
+
+print(sentence)
