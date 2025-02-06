aec63beca91245e85610ea278e17c0c1c8fa898a
Your Name <carlosvanzego96@gmail.com>
2025-02-06 13:53:49 -0600

    sort.py updates and comments

--- sort.py
+++ sort.py
@@ -1,6 +1,6 @@
-# The sort() methods sorts the eoements of a given list in either ascending or descending order. 
+# The sort() method sorts the elements of a given list in either ascending or descending order. 
 # By default, sort() doesn't require any extra parameters. However, it has two optional parameters:
-# reverse - If 'True', the sorted list is reversed (or sorted in Descending order)
+# reverse - If 'True', the sorted list is reversed (or sorted in Descending order).
 # key - function that serves as a key for the sort comparison.
 # Ex.
 # 
@@ -16,10 +16,15 @@ vegetables.sort()
 print(vegetables)
 
 
-mandalorian =["grogu", "darksaber", "force", "space", 'bounty hunter']
+mandalorian = ["grogu", "darksaber", "force", "space", 'bounty hunter']
 
 mandalorian.sort()
 print(mandalorian)
+
+games = ["Link", "Mario", "Zelda"]
+
+games.sort()
+print(games)
 
 
 # To sort a list in descending order, a parameter reverse=True is passed as shown in the below example:
@@ -28,3 +33,8 @@ numbers = [4, 2, 1, 3]
 
 numbers.sort(reverse=True)
 print(numbers)
+
+drinks = ["jameson", "tequila", "rum"]
+
+drinks.sort(reverse=True)
+print(drinks)
