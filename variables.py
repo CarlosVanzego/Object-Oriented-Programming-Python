16bb5107dc3cd8a69bf74cac40fc9368b675a410
Your Name <carlosvanzego96@gmail.com>
2025-02-07 06:53:14 -0600

    variables.py update

--- variables.py
+++ variables.py
@@ -5,21 +5,26 @@ x = 'Los'
 y = 'Carlos'
 z = 'Texas'
 u = 'copilot'
+i = 'value'
 # This varaible "F" is a constant, you know this because I used a capital letter to name the variable. When this is done it is using a naming convention that indicates that the variable should be treated as a constant and not changed. Then I created more constants.
 F = 'Full Metal'
 H = 'Houston'
 G = 'mouse'
+T = 'pass'
 # this is another variable, I called this one "a" and stored the value 4. By doing this I am making "4" of type int (integer). Then I created more variables and stored values in them.
 a = 4
 b = 8
+c = 12
 A = 'alchemist'
 B = 'brotherhood'
 Z = 'zero'
+P = 'pushin'
 # this is a print statement I am using to print the values of the above variables.
 print(F)
 print(A)
 print(z, H, B)
 print(u, G, Z)
+print(i, T, c, P)
 
 
 
