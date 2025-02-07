81e7566aeff7391a333a7897fd85779aeaaa57d9
Your Name <carlosvanzego96@gmail.com>
2025-02-07 17:44:28 -0600

    classesAndObjects.py and comments

---
+++ classesAndObjects.py
@@ -0,0 +1,39 @@
+# Classes and Objects
+# 
+# Classes and Objects:
+# Since Python is an object oriented programming language, almost everything in Python is an object with its own set of attributes and method. A Class is an object constructor or a blueprint for creating objects
+class sampleClass:
+  word = "HelLo"
+
+sampleObject = sampleClass()
+print(sampleObject.word)  
+
+
+
+# The __init__() function:
+# In Python, when defining a class, an __init__() method can be included to initalize the objects attributes.
+
+# For example, in a Student class, attributes like "student_name", "roll_no", and "grade" can be initialized.
+
+# The __init__() method is used to assign values to the objects properties or to perform any other necessary operations at the time of the objects creation.
+# Ex.
+# 
+# I created a class called 'Student'.
+class Student:
+  # I defined the __init__() method, which is the constructor for the class.
+  # It initializes new instances of the 'Student' class with the parameters 'name' and 'age'.
+  def __init__(self, name, age):
+    # I assigned the 'name' parameter to the instance variable 'self.name'.
+    self.name = name
+    # I assigned the 'age' parameter to the instance variable 'self.age'.
+    self.age = age
+
+# I created an instance of the 'Student' class and assigned it to the variable 'studentObject'.
+# I passed the arguments "Carlos" (name) and 28 (age) to the constructor.
+studentObject = Student("Carlos", 28)
+
+# I then print the 'name' attribute of 'studentObject'.
+print(studentObject.name)
+
+# I also print the 'age' attribute of 'studentObject'.
+print(studentObject.age)
