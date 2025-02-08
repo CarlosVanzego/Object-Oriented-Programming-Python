a55e00af5b5f6ef2a8e36f11721f77e1134630ce
Your Name <carlosvanzego96@gmail.com>
2025-02-08 16:32:04 -0600

    heap.py and comments

---
+++ heap.py
@@ -0,0 +1,5 @@
+# Heap
+# 
+# Heap data structure is a complete binary tree that satisfies the heap property.
+# Max Heap: In a Max Heap, the key of each node is always greater than the keys of its child nodes, making the root node's key the largest in the heap.
+# Min Heap: In a Min Heap, the key of each node is always smaller than the keys of its child nodes, resulting in the root node's key being teh smallest in the heap.
