2ec27f7b72a8cc94ff8a77b03cf27de6822895f4
Your Name <carlosvanzego96@gmail.com>
2025-02-08 09:29:20 -0600

    pythonMath.py and comments

---
+++ pythonMath.py
@@ -0,0 +1,27 @@
+# Python Math
+# 
+# Python has a set of built-in math functions which allows you to perform math functions on numbers.
+
+# min() and max() functions:
+# The min() and max() functions can be used to find the lowest or highest value in an interable.
+# Note: iterable in Python can mean anythion that you can loop over and has a list of contents for example a list, tuple, set etc.
+# Ex.
+# 
+getMin = min(5, 10, 25)
+getMax = max(5, 10, 25)
+
+print(getMin)
+print(getMax)
+
+# abs() function:
+# The abs() function returns the absolute (positive) value of the specified number
+# 
+absValue = abs(-7.25)
+
+print(absValue)
+
+# pow() function:
+# The pow(x,y) function returns the value of x to the power of y(x^y)
+x = pow(4,4)
+
+print(x)
