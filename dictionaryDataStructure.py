19534c24e3ec96efee2f834b0655c0a1eab11490
Your Name <carlosvanzego96@gmail.com>
2025-02-08 17:08:02 -0600

    dictionaryDataStructure.py and comments

---
+++ dictionaryDataStructure.py
@@ -0,0 +1,38 @@
+# Dictionary (Data Structure)
+# 
+# A Dictionary is a data structure thatg can map keys to values. It uses a has function to compute anm index into an array of buckets or slots, from which the desired value can be found.
+# Key - a unique identifier used to associate each element (value) in a map.
+# Value - elements associated by keys in a map.
+# Ex.
+# 
+
+# creating a dictionary
+hashMap = {}
+
+# Setting a key-value pair
+hashMap['key1'] = 'value1'
+
+# Getting the size if the dictionary
+size = len(hashMap)
+
+# Accessing a value with a key
+# Replace <key> with the actual key, e.g. 'key1'
+value = hashMap.get('<key>')
+
+# Checking if a key exists in the dictionary
+# Replace <key> with the actual key, e.g. 'key1'
+has_key = '<key>' in hashMap
+
+# Deleting a key-value pair
+# Replace <key> with the actual key, e.g. 'key1'
+hashMap.pop('<key>', None)
+# The None is to avoid KeyError if the key does not exist.
+
+# Clearing the dictionary
+hashMap.clear()
+
+# TIME COMPLEXITY
+# Insertion - Average case O(1), Worst case O(n)
+# Deletion - Average case O(1), Worst case O(n)
+# Search - Average case O(1), Worst case O(n)
+# Space - O(n)
