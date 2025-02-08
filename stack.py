a2ef9127e4c6c060f01d17dae51d8292c8c4c091
Your Name <carlosvanzego96@gmail.com>
2025-02-08 15:48:17 -0600

    stack.py and comments

---
+++ stack.py
@@ -0,0 +1,17 @@
+# Stack
+# 
+# A Stack is a collection of elements, with two principle operations: push, which adds to the collection, and pop, which removes the most recently added element.
+# You can think of a stack as plates stacked on top of each other vertically and you can only touch the top plate.
+# It is a Last in, First-out data structure (LIFO)L: the most recently added object is the first to be removed.
+
+# Basic operations of Stack
+# push() - Inserts an element at the top.
+# pop() - Returns the top element after removing from the stack.
+# imEmpty() - Returns true if the stack is empty.
+# top() - Returns the top element without removing from the stack.
+
+# Time Complexity
+# Access - O(1)
+# Search - O(n)
+# Insert - O(1)
+# Remove - O(1)
