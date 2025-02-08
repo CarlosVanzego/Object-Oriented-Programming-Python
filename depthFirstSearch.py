09992b5ac0528650a45168aa23fb42f62bb761e1
Your Name <carlosvanzego96@gmail.com>
2025-02-08 17:40:19 -0600

    depthFirstSearch.py and comments

---
+++ depthFirstSearch.py
@@ -0,0 +1,14 @@
+# Depth First Search
+# 
+# Depth First Search (DFS) is an algorithm for traversing or searching tree or graph data structures, which uses the idea of backtracking.
+# It explores all the nodes by going forward if possible, or it uses backtracking. The nodes are explored in depth.
+
+# Trewe traversal path: [1,2,3,4,5,6,7]
+
+# Time Complexity:
+# Time complexity is O(V+E), where V ia the number of nodes and E is the nnumber of edges.
+
+# Space complexity:
+# A recursive implementation can have a O(h) space complexity (worst case), where h is the maximal depth of your tree.
+
+# Using an iterative solution with stack is O(|V|)
