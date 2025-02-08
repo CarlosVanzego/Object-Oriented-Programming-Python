c397cd46514e620e4697c07a0c391ee6c5731486
Your Name <carlosvanzego96@gmail.com>
2025-02-08 17:34:57 -0600

    breadthFirstSearch.py and comments

---
+++ breadthFirstSearch.py
@@ -0,0 +1,14 @@
+# Breadth First Search
+# 
+# Breadth First Search (BFS) is an algorithm for traversing or searching tree or graph datg structures.
+# It starts at the tree root (or some arbitrary node of a graph, sometimes referred to as a 'search key'), and explores all of the neighbor nodes at the present depth prior moving on to the nodes at the next depth level.
+
+# Tree traversal path: [1,2,3,4,5,6,7]
+
+
+
+# Time Complexity:
+# Time complexity is O(V+E), where V is the number of nodes and E is the number of edges.
+
+# Space complexity:
+# Space complexity is O(|V|) - since at worst case you need to hold all vertices in the queue.
