d11a737f2d3e025ad7e09da0a5958d786780df48
Your Name <carlosvanzego96@gmail.com>
2025-02-08 11:15:30 -0600

    lists.py updates and comments

--- lists.py
+++ lists.py
@@ -1,3 +1,4 @@
+# Lists 
 # Lists are used to store multiple items in a single variable. The items in a list can be of any data type.
 # To create a list, the elements are placed inside square brackets "[]", seperated by commas. As shown below, lists can contain elements of different types as well as duplicated elements.
 # List items are indexed, the first item has index O, the second item has index 1, etc.
@@ -17,12 +18,12 @@ print(songs[2])
 # List Appending:
 cities = ['new york', 'chicago', 'new orleans']
 
-cities.append("dallas")
+cities.append("san juan")
 print(cities)
 
 # List deletion:
 # The remove() method removes the specified item.
-cities = ["Mayland", "florida", "texas"]
-cities.remove("florida")
+cities = ["Maryland", "florida", "texas"]
+cities.remove("Maryland")
 print(cities)
 
