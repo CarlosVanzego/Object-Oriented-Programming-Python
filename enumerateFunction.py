35836aa67335b7aa892b247e5d3d698ba64376ef
Your Name <carlosvanzego96@gmail.com>
2025-02-08 14:14:02 -0600

    enumerateFunction.py and comments

---
+++ enumerateFunction.py
@@ -0,0 +1,9 @@
+# Enumerate Function
+# 
+# The enumerate() method adds a counter to an interable and returns it as a list or tuples.
+# Ex.
+# 
+languages = ["Python", "JavaScript", "C++", "Ruby"]
+enumeratedList = enumerate(languages)
+
+print(list(enumeratedList))
