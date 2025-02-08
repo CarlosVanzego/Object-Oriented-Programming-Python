d1968192bb3860eeac03fddb456895d9d893de96
Your Name <carlosvanzego96@gmail.com>
2025-02-08 16:23:02 -0600

    binarySearchTree.py and comments

---
+++ binarySearchTree.py
@@ -0,0 +1,11 @@
+# Binary Search Tree
+# 
+# All child nodes in the tree to the right of the current node must be greateer than the current node.
+# All child nodes in the tree to the left of the current node must be lesser than the current node.
+# A binary search tree is a type of binary tree which maintains the property that the value in each node must be greater than or equal to any value stored in the left sub-tree, and less than or equal to any value stored in the right subtree.
+
+# BINARY SEARCH TREE TIME COMPLEXITY
+# Access - O(log(n))
+# Search - O(log(n))
+# Insert - O(log(n))
+# Remove - O(log(n))
