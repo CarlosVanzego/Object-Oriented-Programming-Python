0e9b3eb9cb519b32d15756feff9a3ffea4fbb7a4
Your Name <carlosvanzego96@gmail.com>
2025-02-08 11:31:22 -0600

    whileLoop.py updates and comments

--- whileLoop.py
+++ whileLoop.py
@@ -5,10 +5,10 @@
 # Ex.
 # 
 # I initialized the variable 'z' with the value of 4.
-z = 4
+c = 5
 # I created a while loop that runs as long a 'z' is less than 8.
-while z < 8:
+while c < 11:
   # Inside the loop, I print the current value of 'z'.
-  print(z)
+  print(c)
   # I then increment 'z' by 1 each iteration, stopping before it reaches 8.
-  z += 1
+  c += 1
