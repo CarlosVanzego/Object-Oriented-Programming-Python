9e1f1533058b4f987f62326921889cbeb9f2f898
Your Name <carlosvanzego96@gmail.com>
2025-02-08 13:55:56 -0600

    tuples.py and comments

---
+++ tuples.py
@@ -0,0 +1,20 @@
+# Tuples 
+# 
+# A tuple is similar to a list but the main difference is that it's immutable once it's created. You cannot add or remove items in a tuple once you create it.
+# Tuples are typically used when you want the data to remain unchanged throughout the program. It can be indexed jsut like a list and can have duplicates. It can hold multiple data types as well.
+# To create a Tuple, elements are placed inside these circular parentheses()
+ 
+# Creating a tuple:
+#
+sampleTuple = ("LeBron", "Jordan", "Curry")
+
+print(sampleTuple)
+
+
+# Tuple Length:
+sampleTuple2 = ("playstation", "xbox", "switch")
+print(len(sampleTuple2))
+
+# Accessing tuple elements:
+sampleTuple3 = ("TV", "couch", "desk", "rug")
+print(sampleTuple3[1])
