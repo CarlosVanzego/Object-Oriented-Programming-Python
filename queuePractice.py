fab3309fdba52e7ac8963126e76a39f08c4baa00
Your Name <carlosvanzego96@gmail.com>
2025-02-08 15:53:36 -0600

    queuePractice.py and comments

---
+++ queuePractice.py
@@ -0,0 +1,15 @@
+# Queue
+# 
+# A Queue is a collection of elements, supporting two principle operations: enqueue, which inserts an element into the queue, and dequeue, which removes an element from the queue.
+# First in, first out data structure (FIFO): the oldest added object is the first to be removed.
+
+# Basic operations of Queue in Python
+# put() - Inserts element to the end of the queue.
+# get() - Removes an element from the start of the queue.
+# empty() - Returns true if queue is empty.
+
+# Time Complexity
+# Access - O(n)
+# Search - O(n)
+# Insert - O(1)
+# Remove - O(1)
