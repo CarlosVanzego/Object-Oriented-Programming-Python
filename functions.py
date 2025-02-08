3dc133b8af1898833f3929fa47e9ddce820e06b7
Your Name <carlosvanzego96@gmail.com>
2025-02-08 10:52:06 -0600

    functions.py updates and comments

--- functions.py
+++ functions.py
@@ -1,6 +1,6 @@
 # Functions are predefined tasks created by the programmer to be used later in a program.
 # They help in breaking down the program into smaller, more modular chunks, making the code more organized and reusable.
-# For example you can write a function to draw a square and then call this function whenever you need to draw a square, instead of writing the square-drawing code each time.
+# For example, you can write a function to draw a square and then call this function whenever you need to draw a square, instead of writing the square-drawing code each time.
 # Ex.
 
 def draw_square():
@@ -8,17 +8,17 @@ def draw_square():
 
     draw_square()
 
-def func(a, b):
+def function_add(a, b):
     s = a + b
     return s  
 
-print(func(4, 4)) 
+print(function_add(2, 4)) 
 
-def practice_function():
-    t = 2 * 2
+def multiplication_function():
+    t = 12 * 12
     return t
     
-print(practice_function())
+print(multiplication_function())
 # This is a function called 'spell_name'. I have pass on the line beneath the function declaration to keep it syntactically correct.
 def spell_name():
     pass
