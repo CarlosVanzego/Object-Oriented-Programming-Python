e1efc7e6287dd6673397a050d8d3e7c1d11ae0fb
Your Name <carlosvanzego96@gmail.com>
2025-02-08 11:22:59 -0600

    sets.py updates

--- sets.py
+++ sets.py
@@ -4,20 +4,20 @@
 # Sets do not have a proper order. The items in a set are arranged in a random order.
 # The items in a Set do not have an index.
 # Sets cannot have two items with the same value.
-# Sets are used when you want to store a set of unique values and perform venn diagram like operations on it, for example union, intersection, etc.
+# Sets are used when you want to store a set of unique values and perform venn diagram like operations on it, for example union (method used to combine two or more sets, creating a new set that contains all the unique elements from all the sets involved), intersection (a set method that returns a new set containing elements common to two or more sets), etc.
 # 
 # Sets cannot have duplicates as illustrated below:
 
-cities = {"new york", "chicago", "kansas", "chicago"}
-#  When printing the cities set, the 2nd "chicago" does not show in the terminal. This is because sets cannot have duplicates.
+cities = {"houston", "denver", "kansas", "denver"}
+#  When printing the cities set, the 2nd "denver" does not show in the terminal. This is because sets cannot have duplicates.
 print(cities)
 
 states = {"Texas", "Maryland", "Louisiana", "California"} 
 print(states)
 
-cities = {"new york", "chicago", "kansas"}
-# this prints the length of the cities set. Which is 3.
-print(len(cities))
+cities2 = {"new york", "chicago", "kansas"}
+# this prints the length of the cities2 set. Which is 3.
+print(len(cities2))
 
 set1 = {"a", "b", "c"}
 set2 = {1, 2, 3}
