9dc943d270cfeaa727204ec4a82541bf045503f2
Your Name <carlosvanzego96@gmail.com>
2025-02-08 13:01:54 -0600

    tryExceptBlocks.py updates

--- tryExceptBlocks.py
+++ tryExceptBlocks.py
@@ -2,10 +2,10 @@
 # 
 # Try Except Blocks are used to catch exceptions in Python. The code is divided into two blocks: Try block and an Exception Block.
 
-# The Try Block is used to test a block of code and if there's an error the Exception block is used to handle it.
-# Typically a try except block is used when you are unsue if an exception would come up. You are basically trying to ensure that a piece of code works, but that in the exception that it does not work, the except block handles the error peacefully and your program does not crash and moves on to the next block of code.
+# The Try Block is used to test a block of code and if there's an error, the Exception block is used to handle it.
+# Typically a try except block is used when you are unsure if an exception would come up. You are basically trying to ensure that a piece of code works, but that in the exception that it does not work, the except block handles the error peacefully and your program does not crash and moves on to the next block of code.
 
-# It's good practice to use Try Except blocks whenever you can.
+# It's good practice to use Try Except Blocks whenever you can.
 # Ex.
 # 
 # The try block will generate an exception, because c is not defined, therefore the code execution moves onto the except block:
