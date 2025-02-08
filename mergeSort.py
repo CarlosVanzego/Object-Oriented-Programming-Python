575062c44ccd6d9ba17d761b30f2c24fb37d7eac
Your Name <carlosvanzego96@gmail.com>
2025-02-08 17:29:39 -0600

    mergeSort.py and comments

---
+++ mergeSort.py
@@ -0,0 +1,22 @@
+# Merge Sort
+# 
+# Merge sort is also a divide and conquer algorithm.
+# It uses recursion to keep dividing an array into two halves.
+# At the end, it merges the two sorted halves.
+# Any run of merge sort can be visualized as a tree. The leaves of the tree are the individual elements of the array. Each inner node of the tree cooresponds to merging two smaller arrays into one larger array.
+
+# Divide phase
+# Divide the unsorted list into two sublists of about half the size.
+
+# Conquer phase
+# Sort each of the two sublists recursively until we have list sizes of lenght 1, in which case the list items are returned.
+
+# Combine phase 
+# Join the two sorted sublists back into one nosrted list. When the conquer step reaches the base step and we get two sorted subarrays, we combine the results by creating a sorted array from two sorted arrays.
+
+
+
+# TIME COMPLEXITY
+# Best Case = O(nlog(n))
+# Worst Case = O(nlog(n))
+# Average Case = O(nlog(n))
