c53c177260a6630d7931fbc2abb814910c8ab1ec
Your Name <carlosvanzego96@gmail.com>
2025-02-08 16:06:54 -0600

    tree.py and comments

---
+++ tree.py
@@ -0,0 +1,20 @@
+# Tree 
+# 
+# Each node of the tree will have a root value and a list of referencees to other nodes which are called child nodes.
+# Trees are no cyclic.
+
+# Binary Tree
+# A Binary Tree is a tree data structure in which each node can have at most two children, which are referred to as the left child and right child.
+# Full Tree: A tree in which every node has either 0 or 2 children.
+# Perfect Binary Tree: A binary tree in which all interior nodes have two children and all leaf nodes are at the same level. A perfect Binary Tree has everything filled in. All the leaf nodes are full.
+# Complete Tree: a binary tree in which every level except possibly the last is full and all nodes in the last level are as far left as possible.
+
+# Important Terms In Trees
+# Path - Path refers to the sequence of nodes along the edges of a tree.
+# Root - The node at the top of the tree is called root. There is only one root per tree and one dpath from the root node to any node.
+# Parent - Any node except the root has one edge upward to a node called parent.
+# Child - The node below a given node connected by its edge downward is called its child node.
+# Leaf - The nodes without child nodes.
+# Subtree - Subtree is the descendents of a node.
+# Traversing - Traversing means passing through nodes in an order.
+# Levels - Level of a node represents the generation of a node. If the root node is at level 0, then its next child node is at level 1 its grandchild is at level 2, and so on.
