32fbf2c64d9a1b8a8766508571ce46b07ee56711
Your Name <carlosvanzego96@gmail.com>
2025-02-08 11:32:00 -0600

    whileLoop.py updates

--- whileLoop.py
+++ whileLoop.py
@@ -4,11 +4,11 @@
 # With the while loop, we can execute a set of statements as long as a condition is true.
 # Ex.
 # 
-# I initialized the variable 'z' with the value of 4.
+# I initialized the variable 'c' with the value of 4.
 c = 5
 # I created a while loop that runs as long a 'z' is less than 8.
 while c < 11:
-  # Inside the loop, I print the current value of 'z'.
+  # Inside the loop, I print the current value of 'c'.
   print(c)
-  # I then increment 'z' by 1 each iteration, stopping before it reaches 8.
+  # I then increment 'c' by 1 each iteration, stopping before it reaches 8.
   c += 1
