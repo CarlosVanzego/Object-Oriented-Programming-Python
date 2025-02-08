47d0c69de93cec492534b8f9efde79b9f449b659
Your Name <carlosvanzego96@gmail.com>
2025-02-08 16:45:58 -0600

    primaryQueue.py and comments

---
+++ priorityQueue.py
@@ -0,0 +1,7 @@
+# Priority Queue
+# 
+# A priority queue is a data structure where each element is assigned a priority.
+# Elements with higher priority are served before elements with lower priority.
+# If elements with the same priority occur, they are served according to their order in the queue.
+# Basic operations of a priority queue are inserting, removing, and peeking elements.
+# It is used for load balancing and interrupt hangling in an operating system.
