966ba9de4029bc24c9377a05dfd34c437cafd124
Your Name <carlosvanzego96@gmail.com>
2025-02-08 15:26:07 -0600

    doublyLinkedList.py and comments

---
+++ doublyLinkedList.py
@@ -0,0 +1,5 @@
+# Doubly-Linked List
+# 
+# A linked list in which each node has two pointers, p and n, such that p points to the previous node and n points ot the next node; the last node's n pointer points to null.
+
+# Cicular-linked list: A linked list in which each nodepoints to the next node and the last node points back to the first node.
