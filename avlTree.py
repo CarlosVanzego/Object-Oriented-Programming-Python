9b7a728ba06306779b0040ed076f2f29101534ac
Your Name <carlosvanzego96@gmail.com>
2025-02-08 18:07:36 -0600

    avlTree.py and comments

---
+++ avlTree.py
@@ -0,0 +1,6 @@
+# AVL Tree
+# Thebn main idea with AVL trees is that you always know two things:
+# The order invariant (if x<y then x is left of y)
+# The height of the tree does not differ by more than one level, i.e. the tree is balanced.
+
+# We can convert this from a BST to a height balanced AVL Tree. If we rearrange node "d" and its descendents, we can reformat the exact same BST into an AVL tree, which is balanced.
