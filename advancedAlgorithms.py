7b222093114644eadc9d334638ce05b3355d7b64
Your Name <carlosvanzego96@gmail.com>
2025-02-08 18:27:35 -0600

    advancedAlgorithms.py and comments

---
+++ advancedAlgorithms.py
@@ -0,0 +1,24 @@
+# ADVANCED ALGORITHMS
+
+# Topological Sort
+# The topological sort algorithm tales a directed graph and returns an array of the nodes where each node appears before all the nodes it points to.
+# Topological Sort is the linear ordering of a directed graph's nodes such that for every edge from the node u to node v, u comes before v in the ordering
+
+# Time Complexity: O(|V| + |E|)
+
+
+
+# Dijkstra's Algorithm
+# Dijkstra's Algorithm is an algorithm for finding the shortest path between nodes in a graph.
+
+# Time Complexity: O(|V|^2)
+
+
+
+# Bellman-Ford Algorithm
+# Bellmman-Ford Algorithm is an algorithm thgat computes the shortest paths from a single source node to all other nodes in a weighted graph.
+# Although it is slwoer than Dijkstra's, it is more versatile, as it is capable of handling graphs in which some of the edge weights are negative numbers.
+
+# Time Complexity:
+# Best case: O(|E|)
+# Worst case: O(|V||E|)
