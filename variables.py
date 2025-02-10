5b4123024c417bf970e1449871dc188f0f2b3d4c
Your Name <carlosvanzego96@gmail.com>
2025-02-10 07:21:31 -0600

    variables.py update

--- variables.py
+++ variables.py
@@ -8,21 +8,23 @@ u = 'copilot'
 i = 'value'
 p = 'plant'
 b = 'boxing'
+t = 'type'
 # This varaible "F" is a constant, you know this because I used a capital letter to name the variable. When this is done, it is using a naming convention that indicates that the variable should be treated as a constant and not changed. Then I created more constant variables and assigned them values.
 H = 'Houston'
 G = 'mouse'
 T = 'pass'
 O = 'Orange'
 W = 'Workout'
+B = 'Blue'
 # this is another variable, I called this one "a" and stored the value 4. By doing this I am making "4" of type int (integer). Then I created more variables and stored values in them.
 a = 4
 c = 12
 d = 9
 A = 'alchemist'
-B = 'brotherhood'
 Z = 'zero'
 P = 'pushin'
 F = 'february'
+rand = 'value'
 # Here I created the constatnt variable 'D' and assigned it the boolean value "True".
 D = True
 # this is a print statement I am using to print the values of the above variables.
@@ -33,7 +35,7 @@ print(u, G, Z)
 print(i, T, c, P)
 print(p, O, D)
 print(b, W, d, F)
-
+print(rand, B, t)
 
 
 
