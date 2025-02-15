17205c5c9197b45f9f94fe35c3d403a5d481da57
Your Name <carlosvanzego96@gmail.com>
2025-02-15 01:06:55 -0500

    variables.py update

--- variables.py
+++ variables.py
@@ -10,6 +10,7 @@ p = 'plant'
 b = 'boxing'
 t = 'type'
 f = 'fly'
+hi = "hey there"
 # This varaible "F" is a constant, you know this because I used a capital letter to name the variable. When this is done, it is using a naming convention that indicates that the variable should be treated as a constant and not changed. Then I created more constant variables and assigned them values.
 H = 'Houston'
 G = 'mouse'
@@ -18,6 +19,7 @@ O = 'Orange'
 W = 'Workout'
 B = 'Blue'
 S = 'Southwest'
+I = "I am commited"
 # this is another variable, I called this one "a" and stored the value 4. By doing this I am making "4" of type int (integer). Then I created more variables and stored values in them.
 a = 4
 c = 12
@@ -28,6 +30,7 @@ P = 'pushin'
 F = 'february'
 rand = 'value'
 AIR = 'AIRLINES'
+TY = "To You"
 # Here I created the constatnt variable 'D' and assigned it the boolean value "True".
 D = True
 # this is a print statement I am using to print the values of the above variables.
@@ -40,7 +43,7 @@ print(p, O, D)
 print(b, W, d, F)
 print(rand, B, t)
 print(f, S, AIR)
-
+print(hi, I, TY)
 
 
 # Python infers the variable type since it is a dynamically-typed langauge.
