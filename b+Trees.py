97cdca70618f92fa849d1281598b10ec816a5bff
Your Name <carlosvanzego96@gmail.com>
2025-02-15 10:06:58 -0500

    b+Trees.py update

--- b+Trees.py
+++ b+Trees.py
@@ -6,12 +6,12 @@
 # Data keys are stored in the internal nodes of the tree.
 # If a target key value is less than that of the internal node, the search follows the node just to its left.
 # If a target key value is greater than or equal to that of the internal code, the search follows the node just to its right.
-# The root has a minimum of two childre.
-# A comprehensice scan of all elements requires just one linear pass, as all leaf nodes in a B+ tree are linked.
+# The root has a minimum of two children.
+# A comprehensive scan of all elements requires just one linear pass, as all leaf nodes in a B+ tree are linked.
 
 # Search Operation
-# To find the require record, you ned to execute the binary search on the available records in the Tree. In case of an exact match with the search key, the corresponding record is returned to the user.
+# To find the require record, you need to execute the binary search on the available records in the Tree. In case of an exact match with the search key, the corresponding record is returned to the user.
 
 # In case the exact key is not located by the search in the parent, current, or leaf node, the a "not found message" is displayed to the user.
 
-# The search process can be re-run for better and moer accurate results.
+# The search process can be re-run for better and more accurate results.
