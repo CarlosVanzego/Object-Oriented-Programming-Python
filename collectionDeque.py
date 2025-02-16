b02cb51554146291505b22d6658dcd59d75cb14a
Your Name <carlosvanzego96@gmail.com>
2025-02-16 11:41:38 -0500

    collectionDeque.py update

--- collectionDeque.py
+++ collectionDeque.py
@@ -1,6 +1,6 @@
 # Collection Deque
 # 
-# In Python, a Deque (Doubly Ended Queue) is implemented via the "collections" module. It is more advantageous than a list when swift append and pop operators are required at both ends of the container. This is because a deque ensures O(1) time complexity for these operations, whereas a lsit has an O(n) time complexity.
+# In Python, a Deque (Doubly Ended Queue) is implemented via the "collections" module. It is more advantageous than a list when swift append and pop operators are required at both ends of the container. This is because a deque ensures O(1) time complexity for these operations, whereas a list has an O(n) time complexity.
 # Ex.
 # 
 from collections import deque
