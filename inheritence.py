f0379ea4aec79161469ca14b170a64578ffa9a29
Your Name <carlosvanzego96@gmail.com>
2025-02-17 11:27:58 -0500

    inheritence.py update

--- inheritence.py
+++ inheritence.py
@@ -1,7 +1,7 @@
 # Inheritence in OOP in Python
 
 
-# To simplify this, I can remove the '__init__' method, and then I would need to make an upper level Class 'Pet':
+# To simplify this, I can remove the constuctor '__init__' method, and then I would need to make an upper level Class 'Pet':
 # The 'Pet' Class contains the functinality that both the 'Cat' and 'Dog' Classes have.
 class Pet:
     def __init__(self, name, age):
@@ -33,9 +33,13 @@ class Dog(Pet):
     def speak(self):
         print("Bark")      
 
-p = Pet("Carlos", 28)                  
+# this is an instance of the "Pet" class where I passed the strings "Carlos" and "28".
+p = Pet("Carlos", 28)      
+# then I am calling the show method 
 p.show()
+# this is an instance of the "Cat" class where I passed the strings "Charlie", "6" and "Grey".
 c = Cat("Charlie", 6, "Grey")
+
 # Even though there is no method called 'show' inside of 'Cat', this still prints because it inherits the properties from the 'Pet' Class because it is defined in the 'Cat' Class.
 c.show()
 d = Dog("Ace", 7)
