655a9d5c79e557c4972e76cc06f4a79d46959060
Your Name <carlosvanzego96@gmail.com>
2025-02-17 10:57:53 -0500

    greedyAlgorithms.py update

--- greedyAlgorithms.py
+++ greedyAlgorithms.py
@@ -2,7 +2,7 @@
 # 
 # A greedy algorithm always takes the best/cheapest option available at the moment, even if that might not be the best way to go from an overall point of view.
 # Problems must exhibit two properties in order to implement a Greedy solution:
-\
+
 # Optimal Substructure
 # A problem has an optimal substructure if an optimal solution to the entire problem contains the optimal solutions to the sub-problems.
 
@@ -14,5 +14,5 @@
 # Time complexity: O(|V|^2)
 
 # Kruskal's Algorithm
-# Kruskal's Algorithm is alos a greedy algorithm that finds a minimum spanning tree in a graph, However, in Kruskal's, the graph does not have to be connected.
+# Kruskal's Algorithm is also a greedy algorithm that finds a minimum spanning tree in a graph, However, in Kruskal's, the graph does not have to be connected.
 # Time complexity: O(|E|log|V|)
