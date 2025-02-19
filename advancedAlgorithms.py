c97c41ea5376f764d690280e269083fb20bdd455
Your Name <carlosvanzego96@gmail.com>
2025-02-19 07:27:50 -0600

    advancedAlgorithms.py update

--- advancedAlgorithms.py
+++ advancedAlgorithms.py
@@ -2,7 +2,7 @@
 
 # Topological Sort
 # The topological sort algorithm takes a directed graph and returns an array of the nodes where each node appears before all the nodes it points to.
-# Topological Sort is the linear ordering of a directed graph's nodes such that for every edge from the node u to node v, u comes before v in the ordering
+# Topological Sort is the linear ordering of a directed graph's nodes such that for every edge from the node u to node v, u comes before v in the ordering.
 # It is a method to linearly order the vertices of a directed acyclic graph (DAG) such that for every edge from vertex "u" to vertex "v", "u" appears before "v" in the ordering; essentially, it provides a sequence of nodes where each node is visited only after all its dependencies (nodes that have edges pointing to it) have been visited. 
 
 # Time Complexity: O(|V| + |E|)
@@ -19,7 +19,7 @@
 
 # Bellman-Ford Algorithm
 # Bellmman-Ford Algorithm is an algorithm that computes the shortest paths from a single source node to all other nodes in a weighted graph.
-# It is an algorithm that computes shortest paths from a single source vertex to all of the other vertices in a weighted digraph
+# It is an algorithm that computes shortest paths from a single source vertex to all of the other vertices in a weighted digraph.
 # Although it is slower than Dijkstra's, it is more versatile, as it is capable of handling graphs in which some of the edge weights are negative numbers.
 
 # Time Complexity:
