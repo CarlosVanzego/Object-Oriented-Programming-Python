7f1b4835370fc2563891310a9c69a245a8b70d47
Your Name <carlosvanzego96@gmail.com>
2025-04-02 18:24:59 -0400

    conditionalStatements.py update

--- conditionalStatements.py
+++ conditionalStatements.py
@@ -3,7 +3,7 @@
 # Conditional statements in Python are used to perform different computations based on whether an expression/value evaluates to True or False.
 
 # "if" key word:
-# In this example, I use two variables, c and v, which are used as part of the if statements to test whether v is greater than c. As c is 400, and v is 8 we know that 400 is greater than 8, and so we print that "c is gdsareaqreater than v"
+# In this example, I use two variables, c and v, which are used as part of the if statements to test whether v is greater than c. As c is 400, and v is 8 we know that 400 is greater than 8, and so we print that "v is greater than c"
 
 # "elif" keyword:
 # the 'elif' keyword in python is used to execute a condition if the previous conditions weren't true. It's basically a way of saying that "if the previous conditions were not true, then try this condition."
