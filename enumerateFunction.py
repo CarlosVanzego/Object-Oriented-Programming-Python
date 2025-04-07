feb2e62e44f26cada7d7c03e5e008a5e01c21aef
Your Name <carlosvanzego96@gmail.com>
2025-04-07 18:18:25 -0400

    enumerateFunction.py edits

--- enumerateFunction.py
+++ enumerateFunction.py
@@ -3,13 +3,13 @@
 # The enumerate() method adds a counter to an iterable and returns it as a list or tuples.
 # Ex.
 # 
-languages = ["Python", "JavaScript", "C++", "TypeScript", "Go"]
+languages = ["Python", "JavaScript", "R", "TypeScript", "Bash"]
 enumeratedList = enumerate(languages)
 
 print(list(enumeratedList))
 
 
-tasks = ["Land", "Uber home", "Get dressed", "Get lit"]
+tasks = ["code", "eat", "chill", "sleep"]
 toDoList = enumerate(tasks)
 
 print(list(toDoList))
