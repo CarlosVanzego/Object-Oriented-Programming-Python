c1a716a10e2bd96659c6f2ce30a2faf1aa2da8e1
Your Name <carlosvanzego96@gmail.com>
2025-04-12 08:53:22 -0500

    sets.py practice and update.

--- sets.py
+++ sets.py
@@ -9,34 +9,26 @@
 # Sets are used when you want to store a set of unique values and perform venn diagram like operations on it, for example union (method used to combine two or more sets, creating a new set that contains all the unique elements from all the sets involved), intersection (a set method that returns a new set containing elements common to two or more sets), etc.
 # 
 # Sets cannot have duplicates as illustrated below:
+cities = {"new york", "chicago", "kansas", "chicago"}
 
-cities = {"houston", "denver", "kansas", "denver"}
-#  When printing the cities set, the 2nd "denver" does not show in the terminal. This is because sets cannot have duplicates.
 print(cities)
 
-states = {"Texas", "Maryland", "Louisiana", "California"} 
-print(states)
+# This is a variable I called otherCities where I stored a set of cities.
+otherCities = {"houston", "fort lauderdale", "new orleans"}
 
-cities2 = {"new york", "chicago", "kansas"}
-# this prints the length of the cities2 set. Which is 3.
-print(len(cities2))
+print(len(otherCities))
 
-set1 = {"a", "b", "c"}
+# 
+set1 = {"a", "B", "c"}
 set2 = {1, 2, 3}
-# The union() method is used to combine two or more sets, creating new set that contains all the unique elements from all sets included.
+
 set3 = set1.union(set2)
 print(set3)
 
-fruits = {"blackberry", "apple", "strawberry"}
 
-companies = {"microsoft", "google", "apple"}
 
-presidents = {"Obama", "Trump", "Clinton"}
-print(presidents)
+fruits = {"apple", "banana", "strawberry", "Blueberry", "raspberry", "blackberry"}
+companies = {"microsoft", "IBM", "NASA", "apple"}
 
 common = fruits.intersection(companies)
 print(common)
-
-
-aircrafts = {"airplane", "jets", "spaceship", "airplane", "helicopter"}
-print(aircrafts)
